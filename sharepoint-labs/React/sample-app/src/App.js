b59086fe84b6c85f73c840063e7aa4d93eeb2ab6
Vikash-verma-profile <er.vikashverma@yahoo.com>
2024-04-16 19:11:12 +0530

    added react code

---
+++ sharepoint-labs/React/sample-app/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <>Vikash Verma</>
+  );
+}
+
+export default App;
---
+++ sharepoint-labs/React/sample-app/src/Sample.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Sample extends React.Component{
+    render(){
+        return <h2>Hi I am an example of class component</h2>
+    }
+}
+
+export default Sample;
